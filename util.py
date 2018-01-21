b18fd7c5f1d5dac771335fb4d7e077b983dd0ff5
KnightReigh <12376953+knightReigh@users.noreply.github.com>
2018-01-21 05:15:58 -0600

    Update util.py

--- util.py
+++ util.py
@@ -60,6 +60,8 @@ def remove_nbws(text):
     text = text.replace(u'\u200B', '')
     # NBWS: Non-breaking space
     text = text.replace(u'\xa0', ' ')
+    # HalfWidth fullstop
+    text = text.replace(u'\uff61', ' ')
     # White space
     text = text.replace(u'\t', ' ').replace(u'\r', ' ')
 
